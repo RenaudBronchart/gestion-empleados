47c5d0f75ffe13b490cc75da29997c802c2861d0
Renaud Bronchart <renaudbronchart@gmail.com>
2025-12-02 14:43:21 +0100

    creacion proyect empleado

---
+++ Desktop/gestion-empleados/src/test/java/com/renaud/gestionempleados/GestionEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renaud.gestionempleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmpleadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
